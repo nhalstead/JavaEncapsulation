41964858e21faa00c479bbcb10d96af80cf248cc
Noah Halstead <nhalstead00@gmail.com>
2017-11-07 20:26:07 -0500

    Added the Code for Testing

--- encapsulation/src/main/java/com/sjrobotics/velocity/encapsulation/App.java
+++ encapsulation/src/main/java/com/sjrobotics/velocity/encapsulation/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Game newGame = new Game();
+        System.out.println(newGame.isSetup()?"SETUP Game 1":"AHH NOT SETUP");
+        
+        Game newGame2 = new Game(24);
+        System.out.println(newGame2.isSetup()?"SETUP Game 2":"AHH NOT SETUP");
+        
+        try {
+        	Game newGame3 = new Game(-1);
+        	System.out.println(newGame3.isSetup()?"SETUP Game 3":"AHH NOT SETUP");
+        }
+        catch ( RuntimeException e ){
+        	System.out.println("YOU CAN NOT PUT LOWER THAN 1!!!!");
+        }
     }
 }
--- encapsulation/src/main/java/com/sjrobotics/velocity/encapsulation/game.java
+++ encapsulation/src/main/java/com/sjrobotics/velocity/encapsulation/game.java
@@ -1,5 +1,41 @@
 package com.sjrobotics.velocity.encapsulation;
 
-public class game {
+import java.util.Random;
 
+public class Game {
+	
+	private int hightGuess = 100;
+	private int lowestValue = 0;
+	private int answer = -1;;
+	
+	private int highestValue = 100;
+	private String[] outputC = new String[] { "You WON!", "YOU GOT IT!", "WAY TO GO! YOU WIN!", "WINNER!!!!!", "You Have got It!"};
+	
+	public Game() {
+		this.answer = generateAnswer();
+	}
+	
+	public Game(int inHighGuess) {
+		if(inHighGuess < 1) {
+			throw new IllegalArgumentException();
+		}
+		this.hightGuess = inHighGuess;
+		this.answer = generateAnswer();
+	}
+	
+	private int generateAnswer() {
+		int answer = -1;
+		Random random = new Random();
+		answer = random.nextInt()%(this.hightGuess + 1);
+		answer = Math.abs(answer);
+		return answer;
+	}
+	
+	public boolean isSetup() {
+		if(this.answer != 0 || this.answer != -1) { return true; }
+		return false;
+	}
+	
+	
+	
 }
